d85a45b1130fb5d41aa20f26ba3ba6c6421336b0
Brian Philip Ulaski <brianulaski@gmail.com>
2016-02-03 21:21:52 -0900

    Update BrianUlaski.sh

--- exercise2/BrianUlaski.sh
+++ exercise2/BrianUlaski.sh
@@ -2,55 +2,26 @@
 
 #####################################
 
-#!/bin/bash
+# This script is to be used following Eric's fastq2fasta.sh script.
 
-# takes the first argument to the command line ($1) and saves it into a variable called 'file', which can be accessed at '$file'
-file=$1
+# This script sorts and dereplicates fasta
 
-# runs the command 'basename' to remove the suffix 'fastq' from $file and save the result to a variable called 'outfile'
-outfile=`basename -s fastq $file`
+sed 'N;s/\n/\t/' SRR1145846_S1_L001_R1_001.fasta >> SRR1145846_S1_L001_R1_001.fasta_fake
 
-# appends the new suffix "fastafake" to the output file name
-outfile=$outfile"fastafake"
+sort -n SRR1145846_S1_L001_R1_001.fasta_fake >> SRR1145846_S1_L001_R1_001.fasta
 
+# while read myline; do
+# 	echo "" >> SRR1145846_S1_L001_R1_001.fasta_fake_dup
+# 	echo $myline >> SRR1145846_S1_L001_R1_001.fasta_fake_dup
+# 	echo "" >> SRR1145846_S1_L001_R1_001.fasta_fake_dup
+# 	echo $myline >> SRR1145846_S1_L001_R1_001.fasta_fake_dup
+# done < SRR1145846_S1_L001_R1_001.fasta_sort
 
-# match the single plus sign on a line by itself plus the two lines before it
-# ^ matches the beginning of a line
-# \+ matches a literal plus because a plus sign is also a wildcard regular expression character
-# $ matches the end of a line
-# -B2 prints the two lines Before the match
-# then pipe it into the next command
-# the \ at the end is used be used to break one command over several lines
-grep '^\+$' -B 2 $file | \
+# while read myline; do
+# 	grep -v "^$" SRR1145846_S1_L001_R1_001.fasta_fake_dup >> SRR1145846_S1_L001_R1_001.fasta_fake_nonblank
+# done < SRR1145846_S1_L001_R1_001.fasta_fake_dup
 
-# takes the output from the previous grep and removes the single plus sign (i.e. matches everything but)
-# -v inverts the match
-grep -v '^\+$' | \
-
-# takes the output from the previous grep and removes the dashes added by the first grep '--'
-grep -v '^\-\-$' | \
-
-# tr is the transliterate command, it takes the characters from the first block and replaces each instance with the characters in the second block, one by one, and outputs the result to a new file
-tr '[@: ]' '[>__]' | \
-
-# paste takes the input and concatenates the corresponding lines of the given input files with a tab character by default
-# then we output the text to $outfile
-paste - - > $outfile
-
-# insert script from exercise1 to sort and count uniq
-sed 'N;s/\n/\t/' sort -n SRR1145846_S1_L001_R1_001.fasta
-sort -n SRR1145846_S1_L001_R1_001.fasta > SRR1145846_S1_L001_R1_001.fasta_sort
-
-
-# define new variable 
-savefile=`basename -s fake $outfile`
-
-# to separate fake-fasta files back into real-fasta files use tr to replace tab characters with newline characters
-cat $outfile | tr '\t' '\n' > $savefile
-
-# remove outfile
-rm $outfile
-
-
-
+# while read myline; do
+# 	uniq >> SRR1145846_S1_L001_R1_001.fasta_fake_uniq
+# done < SRR1145846_S1_L001_R1_001.fasta_fake_nonblank
 

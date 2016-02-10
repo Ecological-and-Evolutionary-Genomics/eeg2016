59e763b55999afaf5621c4786e3e8da3b828265b
jwdrew <jwdrew@alaska.edu>
2016-02-10 21:54:15 +0000

    Update Jackson.sh

--- exercise2/Jackson.sh
+++ exercise2/Jackson.sh
@@ -10,12 +10,14 @@
  
  grep '^\+$' -B 2 $file | grep -v '^\+$' | grep -v '^\-\-$' | tr '[@: ]' '[>__]' | paste - - > $outfile
 
- wc *.fastq | sort -n SRR1145846_S1_L001_R1_001.fastafake > assignment2.fasta
+ wc *.fastq 
+ 
+ sort -n SRR1145846_S1_L001_R1_001.fastafake > assignment2.fasta
 
  while read myline; do                        
-  echo $myline >> assignment2.dup.fasta
+  echo "$myline" >> assignment2.dup.fasta
   echo "" >> assignment2.dup.fasta
-  echo $myline >> assignment2.dup.fasta
+  echo "$myline" >> assignment2.dup.fasta
   echo "" >> assignment2.dup.fasta
 done < assignment2.fasta
 

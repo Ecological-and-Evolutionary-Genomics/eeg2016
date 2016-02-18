118c05aadabb735e8c52b4c736ada6ed9d8d6d9a
Brian Philip Ulaski <brianulaski@gmail.com>
2016-02-17 17:24:29 -0900

    Update BrianUlaski.sh

--- exercise2/BrianUlaski.sh
+++ exercise2/BrianUlaski.sh
@@ -52,7 +52,7 @@ savefile=`basename -s fake $outfile`
 cat $outfile | tr '\t' '\n' > $savefile
 
 # count the number of unique sequences and write to a new file
-savefile2= $savefile"_uniq_seqs" 
+savefile2=$savefile"_uniq_seqs" 
 cut -f2 $outfile | sort | grep '[ATGC]' | uniq | wc -l > $savefile2
 
 # after you run this then write the top 100 lines to a new file
